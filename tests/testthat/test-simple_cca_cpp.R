75366c186dd106d111f3af6261a97533b3b2c6dc
liancheng <chenggg@umich.edu>
2023-11-15 22:28:03 -0500

    testthat

--- tests/testthat/test-simple_cca_cpp.R
+++ tests/testthat/test-simple_cca_cpp.R
@@ -1,5 +1,12 @@
 library(testthat)
-test_that("cca_simple function", {
+test_that('stop function',{
+  x <- matrix(rnorm(20), 10, 2)
+  y <- matrix(rnorm(200), 40, 5)
+  expect_error(covarianceMatrix(x, y), "Number of rows of matrices must match!")
+  expect_error(matrixMultiply(x, y), "Incompatible matrix dimensions")
+})
+
+test_that("cca_simple_cpp function", {
   # Create a multivariable data set
   set.seed(123)
   x <- matrix(rnorm(20000), 10000, 2)
@@ -8,8 +15,8 @@ test_that("cca_simple function", {
   # Using R's built-in cancor function to get the baseline results,
   # only the corrected correlations, x and y coefficients are obtained
   benchmark <- cancor(x, y)[1:3]
-  # Get test results using the cca_simple to be tested
-  result <- cca_simple(x, y)
+  # Get test results using the cca_simple_cpp to be tested
+  result <- cca_simple_cpp(x, y)
 
   # Check if the result is a list
   expect_type(result, "list")
@@ -31,9 +38,9 @@ test_that("cca_simple function", {
 
   # Time test the function using system time
   start.time <- Sys.time()
-  result <- cca_simple(x, y)
+  result <- cca_simple_cpp(x, y)
   end.time <- Sys.time()
-  cat('Time for cca_simple: ', end.time - start.time, '\n')
+  cat('Time for cca_simple_cpp: ', end.time - start.time, '\n')
 
   # Time test R's built-in cancor function using system time
   start.time <- Sys.time()
@@ -47,8 +54,8 @@ test_that("cca_simple function", {
   # Using R's built-in cancor function to get the baseline results,
   # only the corrected correlations, x and y coefficients are obtained
   benchmark <- cancor(x, y)[1:3]
-  # Get test results using the cca_simple to be tested
-  result <- cca_simple(x, y)
+  # Get test results using the cca_simple_cpp to be tested
+  result <- cca_simple_cpp(x, y)
 
   # Test to match benchmark results to test results
   expect_equal(result$cor, benchmark$cor, tolerance = 0.0001)
@@ -57,9 +64,9 @@ test_that("cca_simple function", {
 
   # Time test the function using system time
   start.time <- Sys.time()
-  result <- cca_simple(x, y)
+  result <- cca_simple_cpp(x, y)
   end.time <- Sys.time()
-  cat('Time for cca_simple: ', end.time - start.time, '\n')
+  cat('Time for cca_simple_cpp: ', end.time - start.time, '\n')
 
   # Time test R's built-in cancor function using system time
   start.time <- Sys.time()
@@ -73,7 +80,7 @@ test_that("test for unequal input rows", {
   x <- matrix(rnorm(20), nrow = 10)
   y <- matrix(rnorm(30), nrow = 15)  # Different number of rows
 
-  expect_equal(cca_simple(x, y), 'Unequal input row!')
+  expect_equal(cca_simple_cpp(x, y), 'Unequal input row!')
 })
 
 # Test for 0 dimension in x or y
@@ -81,7 +88,5 @@ test_that("test for 0 dimension in x or y", {
   x <- matrix(NA, nrow = 10, ncol = 2)
   y <- matrix(rnorm(20), nrow = 10)
 
-  expect_equal(cca_simple(x, y), '0 dimension in x or y')
+  expect_equal(cca_simple_cpp(x, y), '0 dimension in x or y')
 })
-
-
